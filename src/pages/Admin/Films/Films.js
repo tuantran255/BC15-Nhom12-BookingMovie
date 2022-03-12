ac2f0fb4edfa0fd9df6c1ab7e9380dd3a2df5693
tuantran255 <tranttuan2505@gmail.com>
2022-03-13 06:25:46 +0700

    update showTime

--- src/pages/Admin/Films/Films.js
+++ src/pages/Admin/Films/Films.js
@@ -1,15 +1,23 @@
 import React, { Fragment, useState } from "react";
 import { Input, Button, Tooltip, Table } from "antd";
-import { SearchOutlined, EditOutlined, DeleteOutlined, CalendarOutlined } from "@ant-design/icons";
+import {
+  SearchOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  CalendarOutlined,
+} from "@ant-design/icons";
 import "./Films.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { deletePhim, getAPIDanhSachPhim } from "../../../redux/slices/filmsSlice";
+import {
+  deletePhim,
+  getAPIDanhSachPhim,
+} from "../../../redux/slices/filmsSlice";
 import { NavLink } from "react-router-dom";
 export default function Films() {
   const [valueSearch, setValueSearch] = useState("");
   const dispatch = useDispatch();
-  const { arrDanhSachPhim } = useSelector(state => state.films);
+  const { arrDanhSachPhim } = useSelector((state) => state.films);
 
   useEffect(() => {
     dispatch(getAPIDanhSachPhim());
@@ -34,7 +42,7 @@ export default function Films() {
               alt={films.tenPhim}
               width={100}
               height={100}
-              onError={e => {
+              onError={(e) => {
                 e.target.onError = null;
                 e.target.src = `https//picsum.photos/id/${films.maPhim}/100/100`;
               }}
@@ -57,7 +65,13 @@ export default function Films() {
       width: "45%",
       align: "center",
       render: (text, films) => {
-        return <Fragment>{films.moTa.length > 100 ? films.moTa.substr(0, 100) + "..." : films.moTa}</Fragment>;
+        return (
+          <Fragment>
+            {films.moTa.length > 100
+              ? films.moTa.substr(0, 100) + "..."
+              : films.moTa}
+          </Fragment>
+        );
       },
     },
     {
@@ -67,7 +81,11 @@ export default function Films() {
         let maPhim = films.maPhim;
         return (
           <Fragment>
-            <NavLink key={text} to={`/admin/films/editfilms/${films.maPhim}`} className="text-blue-500 text-2xl p-5">
+            <NavLink
+              key={text}
+              to={`/admin/films/editfilms/${films.maPhim}`}
+              className="text-blue-500 text-2xl p-5"
+            >
               <EditOutlined />
             </NavLink>
             <button
@@ -81,7 +99,14 @@ export default function Films() {
             >
               <DeleteOutlined />
             </button>
-            <NavLink key={1} to={`/admin/films/showtime/${films.maPhim}`} className="text-green-500 text-2xl p-5">
+            <NavLink
+              key={1}
+              to={`/admin/films/showtime/${films.maPhim}`}
+              className="text-green-500 text-2xl p-5"
+              onClick={() => {
+                localStorage.setItem("filmParams", JSON.stringify(films));
+              }}
+            >
               <CalendarOutlined />
             </NavLink>
           </Fragment>
@@ -90,17 +115,19 @@ export default function Films() {
       align: "center",
     },
   ];
-  const handleChangeSearch = e => {
+  const handleChangeSearch = (e) => {
     setValueSearch(e.target.value);
   };
-  const filteredDanhSachPhim = arrDanhSachPhim.filter(person => {
+  const filteredDanhSachPhim = arrDanhSachPhim.filter((person) => {
     return person.tenPhim.toLowerCase().includes(valueSearch.toLowerCase());
   });
   const data = filteredDanhSachPhim;
 
   return (
     <div>
-      <h1 className="text-3xl font-black text-center text-red-600">Quản lý phim:</h1>
+      <h1 className="text-3xl font-black text-center text-red-600">
+        Quản lý phim:
+      </h1>
       <NavLink
         to="/admin/films/addnewfilms"
         className="border-2 px-5 py-2 border-red-500 text-red-500 text-xl rounded-full hover:bg-red-500 hover:text-white"
@@ -108,9 +135,18 @@ export default function Films() {
         Thêm Phim
       </NavLink>
       <div className="my-10 flex">
-        <Input placeholder="Tìm Tên Phim" size="large" bordered="true" onChange={handleChangeSearch} />
+        <Input
+          placeholder="Tìm Tên Phim"
+          size="large"
+          bordered="true"
+          onChange={handleChangeSearch}
+        />
         <Tooltip title="search">
-          <Button icon={<SearchOutlined />} className="my-button-search-films hover:bg-red-500" type="submit" />
+          <Button
+            icon={<SearchOutlined />}
+            className="my-button-search-films hover:bg-red-500"
+            type="submit"
+          />
         </Tooltip>
       </div>
       <Table columns={columns} dataSource={data} rowKey={"maPhim"} />
--- src/pages/Admin/Showtime/ShowTime.js
+++ src/pages/Admin/Showtime/ShowTime.js
@@ -14,6 +14,10 @@ import { useFormik } from "formik";
 import moment from "moment";
 
 export default function ShowTime(props) {
+  let film = {};
+  if (localStorage.getItem("filmParams")) {
+    film = JSON.parse(localStorage.getItem("filmParams"));
+  }
   const formik = useFormik({
     initialValues: {
       maPhim: props.match.params.id,
@@ -35,7 +39,6 @@ export default function ShowTime(props) {
     heThongRapChieu: [],
     cumRapChieu: [],
   });
-  console.log(state);
   useEffect(async () => {
     try {
       let result = await api.get(`/api/QuanLyRap/LayThongTinHeThongRap`);
@@ -88,51 +91,62 @@ export default function ShowTime(props) {
     <div className="container">
       <Form
         name="basic"
-        labelCol={{ span: 8 }}
+        labelCol={{ span: 6 }}
         wrapperCol={{ span: 16 }}
         initialValues={{ remember: true }}
         autoComplete="off"
         onSubmitCapture={formik.handleSubmit}
       >
-        <h3 className="text-2xl">Tạo lịch chiếu</h3>
-        <Form.Item label="Hệ thống rạp">
-          <Select
-            options={state.heThongRapChieu?.map((heThongRap, index) => ({
-              label: heThongRap.tenHeThongRap,
-              value: heThongRap.maHeThongRap,
-            }))}
-            onChange={onChangeHeThongRap}
-            placeholder="Chọn hệ thống rạp"
-          />
-        </Form.Item>
-        <Form.Item label="Cụm rạp">
-          <Select
-            options={state.cumRapChieu?.map((cumRap, index) => ({
-              label: cumRap.tenCumRap,
-              value: cumRap.maCumRap,
-            }))}
-            onChange={onChangeCumRap}
-            placeholder="Chọn cụm rạp"
-          />
-        </Form.Item>
-        <Form.Item label="Ngày chiếu giờ chiếu">
-          <DatePicker
-            format="DD/MM/YYYY hh:mm:ss"
-            showTime
-            onChange={onChangeDate}
-            onOk={onOk}
-          />
-        </Form.Item>
-        <Form.Item label="Giá vé">
-          <InputNumber
-            min={75000}
-            max={150000}
-            onChange={onChangeInputNumber}
-          />
-        </Form.Item>
-        <Form.Item label="Chức năng">
-          <Button htmlType="submit">Tạo lịch chiếu</Button>
-        </Form.Item>
+        <h3 className="text-2xl">Tạo lịch chiếu - {film.tenPhim}</h3>
+        <div className="grid md:grid-cols-12 grid-cols-1 gap-4 py-5">
+          <div className="xl:col-span-3 md:col-span-4">
+            <img
+              src={film.hinhAnh}
+              alt="imgMovie"
+              className="md:w-full w-3/4 mx-auto"
+            />
+          </div>
+          <div className="xl:col-span-9 md:col-span-8">
+            <Form.Item label="Hệ thống rạp">
+              <Select
+                options={state.heThongRapChieu?.map((heThongRap, index) => ({
+                  label: heThongRap.tenHeThongRap,
+                  value: heThongRap.maHeThongRap,
+                }))}
+                onChange={onChangeHeThongRap}
+                placeholder="Chọn hệ thống rạp"
+              />
+            </Form.Item>
+            <Form.Item label="Cụm rạp">
+              <Select
+                options={state.cumRapChieu?.map((cumRap, index) => ({
+                  label: cumRap.tenCumRap,
+                  value: cumRap.maCumRap,
+                }))}
+                onChange={onChangeCumRap}
+                placeholder="Chọn cụm rạp"
+              />
+            </Form.Item>
+            <Form.Item label="Ngày giờ">
+              <DatePicker
+                format="DD/MM/YYYY hh:mm:ss"
+                showTime
+                onChange={onChangeDate}
+                onOk={onOk}
+              />
+            </Form.Item>
+            <Form.Item label="Giá vé">
+              <InputNumber
+                min={75000}
+                max={150000}
+                onChange={onChangeInputNumber}
+              />
+            </Form.Item>
+            <Form.Item label="Chức năng">
+              <Button htmlType="submit">Tạo lịch chiếu</Button>
+            </Form.Item>
+          </div>
+        </div>
       </Form>
     </div>
   );
